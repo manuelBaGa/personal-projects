0b24df259f1d92ce6f15abf9853e0caf6c1c673f
mabalder <mabalder@MABALDER-LAP.mx.oracle.com>
2022-05-08 14:46:59 -0500

    Fixed spider for requests to get metrics and ratios

--- web_scraping/morning_star/tutorial/tutorial/spiders/quotes_spider.py
+++ web_scraping/morning_star/tutorial/tutorial/spiders/quotes_spider.py
@@ -9,65 +9,44 @@ import os
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    def start_requests(self):
-        tickers = ['GOOGL','V','AMZN']
-        ticker_data={}
-        data_directory='C:\\Users\\mabalder\\Desktop\\code_projects\\Projects\\jupyterlab\\jupyter_notebooks\\Data\\fundamental_analysis\\ticker_ratios\\'
+    data_directory='C:\\Users\\mabalder\\Desktop\\code_projects\\Projects\\jupyterlab\\jupyter_notebooks\\Data\\fundamental_analysis\\ticker_ratios\\'
+    tickers_symbols = ['V','TMUS','GOOGL','VOO']        
 
-        for ticker in tickers:
-            ticker_data[ticker]={}
-            ratio_url='http://financials.morningstar.com/finan/financials/getKeyStatPart.html?&t='+ticker+'&region=usa&culture=en-US'
-            metric_url='http://financials.morningstar.com/valuate/current-valuation-list.action?=&t='+ticker
+    def start_requests(self):
+        
+        for symbol in self.tickers_symbols:
+            ratio_url='http://financials.morningstar.com/finan/financials/getKeyStatPart.html?&t='+symbol+'&region=usa&culture=en-US'
+            metric_url='http://financials.morningstar.com/valuate/current-valuation-list.action?=&t='+symbol
             
-            ratios=scrapy.Request(url=ratio_url, callback=self.parse_ratios,cb_kwargs=dict(ticker_name=ticker))
-            ticker_data[ticker]['ratios']=yield scrapy.Request(url=ratio_url, callback=self.parse_ratios) #, cb_kwargs=dict(ticker_name=ticker))         
-            #print(ticker_data[ticker]['ratios'])
-            ticker_data[ticker]['metrics']=yield scrapy.Request(url=metric_url, callback=self.parse_metrics) #, cb_kwargs=dict(ticker_name=ticker))
-            #print(ticker_data[ticker]['metrics'])
-            #yield scrapy.Request(url=ratio_url, callback=self.parse_metrics(ticker))
-            #Creating json files to store ratios
-            with open(data_directory+ticker+'.json', 'w') as fp:
-                json.dump(ticker_data, fp,indent=4)
-            yield ratios
+            item=dict()
+            item['ticker']=symbol
+            yield scrapy.Request(url=ratio_url, callback=self.parse_ratios,meta={'item':item,'metricURL':metric_url})
+
 
     def parse_ratios(self, response):
-        ratios_dict={}
-        #Extracting tickers name from the response url
-        #ticker_name=ticker_name#re.split("=|&",response.url)[2]
-        #Extracting ratios names and values 
+        general_dict=response.meta['item']
+        metric_url=response.meta['metricURL']
+        ratios_dict={} 
         financial_ratios_names=response.xpath("//tbody/tr/th/text()").getall()
         #Creating dictionary with ratios names and values
         for ratio_name in financial_ratios_names:
             name_sq=ratio_name.replace('\'','')
             ratios_dict[name_sq]=response.xpath(f"//th[text()='{name_sq}']/../td[last()]/text()").get()
+        general_dict['ratios']=ratios_dict
 
-        #data_directory='C:\\Users\\mabalder\\Desktop\\code_projects\\Projects\\jupyterlab\\jupyter_notebooks\\Data\\fundamental_analysis\\ticker_ratios\\'
-        
-        #with open(data_directory+ticker_name+'.json', 'w') as fp:
-        #        json.dump(ratios_dict, fp,indent=4)
-
-
-        yield ratios_dict
+        yield scrapy.Request(url=metric_url, callback=self.parse_metrics,meta={'item':general_dict})
         
     def parse_metrics(self, response):
-        metrics={}
-        #Extracting tickers name from the response url
-        #ticker_name=ticker#re.split("=|&",response.url)[2]
-        #Extracting ratios names and values 
-        #financial_ratios_names=response.xpath("//div[@id='\\\"tab-financial\\\"']//tbody/tr/th/text()").getall()[:24]
+        general_dict=response.meta['item']
+        metrics_dict={}
         metrics_names=response.xpath("//tbody/tr/th/text()").getall()[:-1]
-        #financial_ratios_values= response.xpath("//div[@id='\\\"tab-financial\\\"']//tbody/tr/td[last()]/text()").getall()[:24]
-        #Creating dictionary with ratios names and values
         for metric_name in metrics_names:
             name_sq=metric_name.replace('\'','')
-            metrics_value=1#response.xpath(f"//th[text()='{name_sq}']/../td[last()]/text()").get()
-            metrics[name_sq]=metrics_value
-            yield dict(name_sq = metrics_value)
+            metrics_value=response.xpath(f"//th[text()='{name_sq}']/../td[4]/text()").get()
+            metrics_dict[name_sq]=metrics_value
+        general_dict['metrics']=metrics_dict
+
+        with open(self.data_directory+general_dict['ticker']+'.json', 'w') as fp:
+                json.dump(general_dict, fp,indent=4)
+        return general_dict
         
-        
-       
-        #Creating json files to store ratios
-        #data_directory='C:\\Users\\mabalder\\Desktop\\code_projects\\Projects\\jupyterlab\\jupyter_notebooks\\Data\\fundamental_analysis\\ticker_ratios\\'
-        #json_file=open(data_directory+ticker_name+".json","w")
-        #temporal_file=open(os.getcwd()+'\\scrapped_data\\'+ticker_name+".json","w")
-        #json.dump(ratios,json_file)                    
